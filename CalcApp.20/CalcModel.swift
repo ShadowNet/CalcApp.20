5566f4261170d9b27551dcc4b66ca10494108fbf
Redghy Jean <33237411+ShadowNet@users.noreply.github.com>
2022-04-28 10:07:39 -0400

    Work In Progress

---
+++ CalcApp.20/CalcModel.swift
@@ -0,0 +1,8 @@
+//
+//  CalcModel.swift
+//  CalcApp.20
+//
+//  Created by Redghy on 4/28/22.
+//
+
+import Foundation
--- CalcApp.20/SceneDelegate.swift
+++ CalcApp.20/SceneDelegate.swift
@@ -11,12 +11,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let rootVC = ViewController()
+       // let navC = UINavigationController(rootViewController: rootVC)
+        self.window = UIWindow(windowScene: windowScene)
+       // self.window?.rootViewController = navC
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -50,3 +55,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 }
 
+    
+        
