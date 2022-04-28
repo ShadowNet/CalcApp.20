e7ffcabb1c7bb07d4a168b78f8bc25c2aa13fa51
Redghy Jean <33237411+ShadowNet@users.noreply.github.com>
2022-04-28 19:47:45 -0400

    Work in progress

--- CalcApp.20/SceneDelegate.swift
+++ CalcApp.20/SceneDelegate.swift
@@ -11,18 +11,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        let rootVC = ViewController()
-       // let navC = UINavigationController(rootViewController: rootVC)
-        self.window = UIWindow(windowScene: windowScene)
-       // self.window?.rootViewController = navC
-        self.window?.makeKeyAndVisible()
-    }
+    func scene(_ scene: UIScene,
+                      willConnectTo session: UISceneSession,
+                      options connectionOptions: UIScene.ConnectionOptions) {
+               guard let windowScene = (scene as? UIWindowScene) else { return }
+
+               let window = UIWindow(windowScene: windowScene)
+               window.rootViewController = ViewController()
+               self.window = window
+               window.makeKeyAndVisible()
+           }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
--- CalcApp.20/ViewController.swift
+++ CalcApp.20/ViewController.swift
@@ -9,216 +9,262 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var CalcMath: UILabel!
-    
-    @IBOutlet weak var CalcResults: UILabel!
-    
+    var tableView: UIView!
+    var resultLabel: UILabel!
+    weak var CalcMath: UILabel!
+    weak var CalcResults: UILabel!
     var workings: String = ""
-    
-    override func viewDidLoad()
-        {
-            super.viewDidLoad()
-            clearAll()
-        }
-        
-        func clearAll()
-        {
-            workings = ""
-            CalcMath.text = ""
-            CalcResults.text = ""
-        }
+       
+       override func viewDidLoad() {
+           
+           super.viewDidLoad()
+           
+           tableView = UIView()
+           tableView.backgroundColor = .white
+           self.view.addSubview(tableView)
+           
+           func clearAll()
+           {
+               workings = ""
+               CalcMath.text = ""
+               CalcResults.text = ""
+           }
 
-        @IBAction func equalsTap(_ sender: Any)
-        {
-            if(validInput())
-            {
-                let checkedWorkingsForPercent = workings.replacingOccurrences(of: "%", with: "*0.01")
-                let expression = NSExpression(format: checkedWorkingsForPercent)
-                let result = expression.expressionValue(with: nil, context: nil) as! Double
-                let resultString = formatResult(result: result)
-                CalcResults.text = resultString
-            }
-            else
-            {
-                let alert = UIAlertController(
-                    title: "Invalid Input",
-                    message: "Calculator unable to do math based on input",
-                    preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "Okay", style: .default))
-                self.present(alert, animated: true, completion: nil)
-            }
-        }
-        
-        func validInput() ->Bool
-        {
-            var count = 0
-            var funcCharIndexes = [Int]()
-            
-            for char in workings
-            {
-                if(specialCharacter(char: char))
-                {
-                    funcCharIndexes.append(count)
-                }
-                count += 1
-            }
-            
-            var previous: Int = -1
-            
-            for index in funcCharIndexes
-            {
-                if(index == 0)
-                {
-                    return false
-                }
-                
-                if(index == workings.count - 1)
-                {
-                    return false
-                }
-                
-                if (previous != -1)
-                {
-                    if(index - previous == 1)
-                    {
-                        return false
-                    }
-                }
-                previous = index
-            }
-            
-            return true
-        }
-        
-        func specialCharacter (char: Character) -> Bool
-        {
-            if(char == "*")
-            {
-                return true
-            }
-            if(char == "/")
-            {
-                return true
-            }
-            if(char == "+")
-            {
-                return true
-            }
-            return false
-        }
-        
-        func formatResult(result: Double) -> String
-        {
-            if(result.truncatingRemainder(dividingBy: 1) == 0)
-            {
-                return String(format: "%.0f", result)
-            }
-            else
-            {
-                return String(format: "%.2f", result)
-            }
-        }
-        
-        @IBAction func allClearTap(_ sender: Any)
-        {
-            clearAll()
-        }
-        
-        @IBAction func backTap(_ sender: Any)
-        {
-            if(!workings.isEmpty)
-            {
-                workings.removeLast()
-                CalcMath.text = workings
-            }
-        }
-        
-        func addToWorkings(value: String)
-        {
-            workings = workings + value
-            CalcMath.text = workings
-        }
-        
-        @IBAction func percentTap(_ sender: Any)
-        {
-            addToWorkings(value: "%")
-        }
-        
-        @IBAction func divideTap(_ sender: Any)
-        {
-            addToWorkings(value: "/")
-        }
-        
-        @IBAction func timesTap(_ sender: Any)
-        {
-            addToWorkings(value: "*")
-        }
-        
-        @IBAction func minusTap(_ sender: Any)
-        {
-            addToWorkings(value: "-")
-        }
-        
-        @IBAction func plusTap(_ sender: Any)
-        {
-            addToWorkings(value: "+")
-        }
-        
-        @IBAction func decimalTap(_ sender: Any)
-        {
-            addToWorkings(value: ".")
-        }
-        
-        @IBAction func zeroTap(_ sender: Any)
-        {
-            addToWorkings(value: "0")
-        }
-        
-        @IBAction func oneTap(_ sender: Any)
-        {
-            addToWorkings(value: "1")
-        }
-        
-        @IBAction func twoTap(_ sender: Any)
-        {
-            addToWorkings(value: "2")
-        }
-        
-        @IBAction func threeTap(_ sender: Any)
-        {
-            addToWorkings(value: "3")
-        }
-        
-        @IBAction func fourTap(_ sender: Any)
-        {
-            addToWorkings(value: "4")
-        }
-        
-        @IBAction func fiveTap(_ sender: Any)
-        {
-            addToWorkings(value: "5")
-        }
-        
-        @IBAction func sixTap(_ sender: Any)
-        {
-            addToWorkings(value: "6")
-        }
-        
-        @IBAction func sevenTap(_ sender: Any)
-        {
-            addToWorkings(value: "7")
-        }
-        
-        @IBAction func eightTap(_ sender: Any)
-        {
-            addToWorkings(value: "8")
-        }
-        
-        @IBAction func nineTap(_ sender: Any)
-        {
-            addToWorkings(value: "9")
-        }
-    }
+
+           tableView.translatesAutoresizingMaskIntoConstraints = false
+           
+           tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+           tableView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+           tableView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier:1).isActive = true
+           tableView.heightAnchor.constraint(equalTo: self.view.heightAnchor,  multiplier: 1).isActive = true
+           
+           
+           setupNumberPad()
+       }
+
+   
+       private func setupNumberPad(){
+           let buttonSize: CGFloat = view.frame.size.width / 4
+           let singleButton = UIButton(frame: CGRect(x:buttonSize * CGFloat(2), y: view.frame.size.height-buttonSize, width: buttonSize, height: buttonSize))
+           let zeroButton = UIButton(frame: CGRect(x:0, y: view.frame.size.height-buttonSize, width:   buttonSize*2, height: buttonSize))
+           zeroButton.setTitle("0", for: .normal)
+           zeroButton.backgroundColor = .black
+           tableView.addSubview(zeroButton)
+           singleButton.backgroundColor = .black
+           singleButton.setTitle(".", for: .normal)
+           tableView.addSubview(singleButton)
+
+           var workings: String = ""
+           
+            func viewDidLoad()
+               {
+                   super.viewDidLoad()
+                   clearAll()
+               }
+               
+           func clearAll()
+           {
+               workings = ""
+               CalcMath.text = ""
+               CalcResults.text = ""
+           }
+
+                func equal(_ sender: Any)
+               {
+                   
+                   if(validInput())
+                   {
+                       let checkedWorkingsForPercent = workings.replacingOccurrences(of: "%", with: "*0.01")
+                       let expression = NSExpression(format: checkedWorkingsForPercent)
+                       let result = expression.expressionValue(with: nil, context: nil) as! Double
+                       let resultString = formatResult(result: result)
+                       CalcResults.text = resultString
+                   }
+                   else
+                   {
+                       let alert = UIAlertController(
+                           title: "Invalid Input",
+                           message: "Calculator unable to compute due to input",
+                           preferredStyle: .alert)
+                       alert.addAction(UIAlertAction(title: "Okay", style: .default))
+                       self.present(alert, animated: true, completion: nil)
+                   }
+               }
+               
+               func validInput() ->Bool
+               {
+                   var count = 0
+                   var funcCharIndexes = [Int]()
+                   
+                   for char in workings
+                   {
+                       if(specialCharacter(char: char))
+                       {
+                           funcCharIndexes.append(count)
+                       }
+                       count += 1
+                   }
+                   
+                   var previous: Int = -1
+                   
+                   for index in funcCharIndexes
+                   {
+                       if(index == 0)
+                       {
+                           return false
+                       }
+                       
+                       if(index == workings.count - 1)
+                       {
+                           return false
+                       }
+                       
+                       if (previous != -1)
+                       {
+                           if(index - previous == 1)
+                           {
+                               return false
+                           }
+                       }
+                       previous = index
+                   }
+                   
+                   return true
+               }
+               
+               func specialCharacter (char: Character) -> Bool
+               {
+                   if(char == "*")
+                   {
+                       return true
+                   }
+                   if(char == "/")
+                   {
+                       return true
+                   }
+                   if(char == "+")
+                   {
+                       return true
+                   }
+                   return false
+               }
+               
+               func formatResult(result: Double) -> String
+               {
+                   if(result.truncatingRemainder(dividingBy: 1) == 0)
+                   {
+                       return String(format: "%.0f", result)
+                   }
+                   else
+                   {
+                       return String(format: "%.2f", result)
+                   }
+               }
+               
+               func allClearTap(_ sender: Any)
+               {
+                   clearAll()
+               }
+               
+               func backTap(_ sender: Any)
+               {
+                   if(!workings.isEmpty)
+                   {
+                       workings.removeLast()
+                       CalcMath.text = workings
+                   }
+               }
+               
+               func addToWorkings(value: String)
+               {
+                   workings = workings + value
+                   CalcMath.text = workings
+               }
+               
+              
+           func percentTap(_ sender: Any) {
+               addToWorkings(value: "%")
+           }
+           
+           func divideTap(_ sender: Any) {
+               addToWorkings(value: "/")
+           }
+           
+               
+           func multiplyTap(_ sender: Any) {
+               addToWorkings(value: "*")
+           }
+           
+           func minusTap(_ sender: Any) {
+               addToWorkings(value: "-")
+           }
+           
+               
+           func plusTap(_ sender: Any) {
+               addToWorkings(value: "+")
+           }
+           
+               
+           func decimalTap(_ sender: Any) {
+               addToWorkings(value: ".")
+           }
+           
+              
+        func zeroTap(_ sender: Any) {
+               addToWorkings(value: "0")
+           }
+           
+               
+            func oneTap(_ sender: Any) {
+               addToWorkings(value: "1")
+           }
+           
+              
+           func twoTap(_ sender: Any) {
+               addToWorkings(value: "2")
+               
+           }
+           
+               
+           func threeTap(_ sender: Any) {
+               addToWorkings(value: "3")
+           }
+           
+              
+           func fourTap(_ sender: Any) {
+               addToWorkings(value: "4")
+           }
+           
+              
+            func fiveTap(_ sender: Any) {
+                  addToWorkings(value: "5")
+               }
+           
+              
+               func sixTap(_ sender: Any) {
+                   addToWorkings(value: "6")
+               }
+           
+              
+               func sevenTap(_ sender: Any) {
+                   addToWorkings(value: "7")
+               }
+           
+               
+           func eightTap(_ sender: Any) {
+               addToWorkings(value: "8")
+           }
+           
+           func nineTap(_ sender: Any) {
+               addToWorkings(value: "9")
+           }
+           
+           }
+
+          // resultLabel.frame = CGRect(x: 0, y: view.frame.origin.y - 100, width:view.frame.size.width, height: 100)
+          // view.addSubview(resultLabel)
+       }
+   
 /*
     override func viewDidLoad() {
         super.viewDidLoad()
